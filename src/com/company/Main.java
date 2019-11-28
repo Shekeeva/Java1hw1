13234778273e65850a894ee991cad1ad3656b980
Baktygul <bbakulya1989@mail.ru>
2019-11-28 21:30:07 +0600

    Java 1 h/w

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Привет, Мир!");
+        int varOfPerson = 30;
+        System.out.println(varOfPerson);
+        varOfPerson = 60;
+        System.out.println(varOfPerson);
+        System.out.println("Привет, Мир!" + varOfPerson);
+        final String hi = "Привет!";
+        System.out.println(hi);
+    }
+}
